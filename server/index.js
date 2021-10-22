30893a073e577f86c02bfe4c7f7eceb9d5f4056e
Ubuntu <ubuntu@ip-172-31-8-226.ap-northeast-2.compute.internal>
2021-10-22 09:32:10 +0000

    ec2에서 수정한 서버 코드

--- server/index.js
+++ server/index.js
@@ -1,24 +1,25 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
 app.use(cors({
-  origin : ['http://localhost:3000'],
+  origin : 'http://deploy-practice-client.s3-website.ap-northeast-2.amazonaws.com',
   credentials : true
 }));
-app.use(express.json());
 app.use(cookieParser());
-app.get('/', (_, res) => res.send('Hello World!'));
+app.get('/', (_, res) => res.send('Hello World!!!'));
 app.post('/login', (req, res) => {
   res.cookie('Cookie1', 'This is cookie', {
     httpOnly : true,
     maxAge: 60*60*1000
   });
-  res.header({'authorization': 'Bearer Token'}).json({message : 'Login OK!!!', loginInfo : req.body, token : 'token'});
+  res.json({message : 'Login OK!!!', loginInfo : req.body, token : 'token'});
 });
 app.get('/check', (req, res) => {
   console.log(req);
   res.send('plz everything is alright');
 })
 const port = 80;
-app.listen(port, () => console.log(`Listening at ==> port : {port}`));
+app.listen(port, () => console.log(`Listening at ==> port : ${port}`));
